409fa2b0b6b35c0c0dd6987aba89b3a8fa1321b8
yebink <yebin825@gmail.com>
2020-10-14 22:48:00 +0900

    jpa-querydsl Member 접근 QueryDSL 메소드 생성

--- jpa-querydsl/src/main/java/com/ellyspace/jpaquerydsl/repository/MemberQueryRepository.java
+++ jpa-querydsl/src/main/java/com/ellyspace/jpaquerydsl/repository/MemberQueryRepository.java
@@ -1,14 +1,24 @@
 package com.ellyspace.jpaquerydsl.repository;
 
+import com.ellyspace.jpaquerydsl.domain.Member;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
+import static com.ellyspace.jpaquerydsl.domain.QMember.member;
+
 @Repository
 public class MemberQueryRepository {
-    private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory jpaQueryFactory;
 
-    public MemberQueryRepository(JPAQueryFactory queryFactory) {
-        this.queryFactory = queryFactory;
+    public MemberQueryRepository(JPAQueryFactory jpaQueryFactory) {
+        this.jpaQueryFactory = jpaQueryFactory;
     }
 
+    public QueryResults<Member> findMembersByTeam(Long teamId) {
+        return jpaQueryFactory.selectFrom(member)
+                .where(member.team.id.eq(teamId))
+                .limit(3)
+                .fetchResults();
+    }
 }
